d06ef9b57f44202b1172126e643b652d4f315d0c
manyeka-petros <manyekapetros29@gmail.com>
2022-12-16 07:52:23 -0800

    initial commit

---
+++ src/main/java/com/groupnumber8/onlinenoticeboard/entities/Announcement.java
@@ -0,0 +1,23 @@
+package com.groupnumber8.onlinenoticeboard.entities;
+
+import lombok.*;
+
+import javax.persistence.*;
+import java.util.Date;
+
+
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+public class Announcement {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    private String title;
+    @Column(length = 1000000)
+    private String message;
+    private String postedOn;
+    private String expireOn;
+}
